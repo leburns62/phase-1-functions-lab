2733fbf4f3d7eb039d409c80f2f08e9cbb6bca3c
Levi Hutchinson Burns <leburns62@gmail.com>
2022-04-29 12:59:46 -0400

    commit tree 046d79b7b4e541faabddb32e2e911eb7a2105975

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+    return Math.abs(42 - streetNumber);
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return distanceFromHqInBlocks(streetNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.max(start, destination) - Math.min(start, destination)) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  const feet = distanceTravelledInFeet(start, destination);
+
+    if (feet <= 400) return 0;
+    else if (feet > 400 && feet < 2000)
+        return (feet - 400) * .02;
+    else if (feet >= 2000 && feet < 2500)
+        return 25;
+    else if (feet >= 2500)
+        return "cannot travel that far";
+}
